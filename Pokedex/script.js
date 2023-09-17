e22bc9d0ed451b2286a512bb74263096ffa3de9c
lucas-catto <lucascattosilva2018@gmail.com>
2023-09-16 20:24:10 -0700

    Legal por enquanto, vou melhorar mais. Vou subir para testar no Github Pages.

---
+++ Pokedex/script.js
@@ -0,0 +1,32 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    fetch('0_Data/pokemons.json')
+
+        .then(response => response.json())
+        .then(data => {
+            
+            let pokes = '';
+
+            data.Pokemon.forEach(poke => {
+
+                pokes += `
+                    <div class="card">
+                        <div class="Image">
+                            <img src="${poke.Imagem}">
+                        </div>
+                                              
+                        <div id="Nome"      >${          poke.Nome      }</div>
+                        <div id="Especie"   >Espécie: ${ poke.Especie   }</div>
+                        <div id="Tipo"      >Tipo: ${    poke.Tipo      }</div>
+                        <div id="Altura"    >Altura: ${  poke.Altura    }</div>
+                        <div id="Peso"      >Peso: ${    poke.Peso      } Kg</div>
+                        <div id="Descricao" >${          poke.Descricao }</div>
+                        
+                    </div>
+                `;
+            });
+            document.getElementById('main').innerHTML = pokes;
+        })
+        .catch(error = console.error('Erro ao buscar personagens:', error));
+});
